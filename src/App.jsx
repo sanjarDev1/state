f5e47d0ba51d53a2c4666a307f6fb4f8bd713e41
sanjarDev1 <sanjarbekqobulov02@gmail.com>
2021-12-08 14:50:05 +0500

    task

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ class Student extends React.Component{
     super(props);
     this.state = {
       count: 1,
+      name: '',
     };
   }
   render(){
    let count =0;
-     const increment = () =>{
-       this.setState({count: this.state.count + 1})
+     const increment = (cnt) =>{
+       this.setState({count: this.state.count + cnt})
       console.log('increment',count);
     }
     
@@ -18,13 +19,18 @@ class Student extends React.Component{
       this.setState({count: this.state.count - 1})
       console.log('decrement',count);
     }
-   
-   
+
+    const onChange = (e)=>{
+      this.setState({name: e.target.value})
+    }
     return (
       <div>
         <h1> Hi State {this.state.count}</h1>
-        <button onClick={increment}>+</button>
+        <h1>Title: {this.state.name}</h1>
+        <button onClick={()=>increment(2)}>+</button>
         <button onClick={decrement}>-</button>
+        <br />
+        <input onChange={onChange} type="text" />
       </div>
     )
   }
