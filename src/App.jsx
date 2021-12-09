38172cc63bcad68135afa5a6ee242396e07a39be
sanjarDev1 <sanjarbekqobulov02@gmail.com>
2021-12-09 23:30:54 +0500

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
 import React from 'react';
 
 class Student extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      count: 1,
-      name: '',
-    };
-  }
-  render(){
-   let count =0;
-     const increment = (cnt) =>{
-       this.setState({count: this.state.count + cnt})
-      console.log('increment',count);
-    }
-    
-    const decrement = () =>{
-      this.setState({count: this.state.count - 1})
-      console.log('decrement',count);
-    }
-
-    const onChange = (e)=>{
-      this.setState({name: e.target.value})
-    }
+  
     return (
       <div>
         
